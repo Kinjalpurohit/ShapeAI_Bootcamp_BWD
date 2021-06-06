2040e37d709d3de76358c0104d6a9e9cf406472c
Kinjalpurohit <85434090+Kinjalpurohit@users.noreply.github.com>
2021-06-06 14:30:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './component/App.jsx';
+import App from './component/App.jsx';
+
+ReactDOM.render(
+  <div>
+    <App/>
+  </div>,
+  document.getElementById('root')
+);
